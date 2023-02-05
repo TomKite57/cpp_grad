5c051799305bd71af4e8d05004d94c59fcca7eb6
TKite <thomas.kite@manchester.ac.uk>
2023-02-05 10:38:24 +0000

    Implemented Andrej's example as a sanity check

--- main.cpp
+++ main.cpp
@@ -8,18 +8,21 @@ int main()
 {
     auto a = Value<double>(2.0);
     auto b = Value<double>(-3.0);
-    //auto c = Value<double>(10.0);
-    auto c = a / b - 3.0;
+    auto c = Value<double>(10.0);
+    auto e = a * b;
+    auto d = e + c;
+    auto f = Value<double>(-2.0);
+    auto L = d * f;
 
-    auto d = pow(c, 2.0);
-    //auto d = c * 3.0;
-
-    d.backward();
+    L.backward();
 
     std::cout << a << "\n"
               << b << "\n"
               << c << "\n"
-              << d << "\n";
+              << d << "\n"
+              << e << "\n"
+              << f << "\n"
+              << L << "\n";
 
     return 0;
 }
